b344bb770b9d3dd3780ece146966d60f65961061
ShunyaSuzuki <anyushu2017@gmail.com>
2020-04-24 16:42:33 +0900

    update

--- header.php
+++ header.php
@@ -8,102 +8,36 @@
 <body>
 <!-- ヘッダー -->
 <header class="header sticky-top">
-
-<h1 class="mb-0 bg-info header--h1">
-<span class="container d-block">大阪の空き家買取なら【空き家買取さん】にお任せください。無料査定・高価買取・売却の相談</span>
+<div class="header__top">
+<div class="container">
+<h1 class="header__top--logo">
+<a class="text-body m-plus" href="<?php e_home_url(); ?>">相続不動産サポートセンター</a>
 </h1>
-
-<div class="container position-relative">
-
-<div class="d-flex header__main">
-
-<a class="header__main__logo" href="<?php e_home_url(); ?>">
-<img class="img-fluid header__main__logo--img" src="<?php e_img_url(); ?>/head_logo.png" alt="<?php bloginfo('name'); ?>">
+<div class="header__top--toho m-plus">谷町四丁目駅から<br>徒歩2分</div>
+<a class="header__top__tel" href="tel:0120200567">
+<span class="header__top__tel--icon"><img class="img-fluid" src="<?php e_img_url(); ?>/icon_free.png" alt="フリーダイヤル"></span>
+<span class="header__top__tel--num">0120-200-567</span>
+<span class="header__top__tel--txt">営業時間：10:00 〜19:00｜定休日：水曜日</span>
 </a>
-
-<div class="header__main__tel d-md-flex">
-
-<div class="header__main__tel__img">
-<img class="img-fluid" src="<?php e_img_url(); ?>/head_point.png" alt="無料査定">
+<div class="header__top__tel--btn">
+<a class="btn btn-primary font-weight-bold text-white" href="<?php e_home_url(); ?>/contact/"><i class="far fa-envelope mr-2"></i>無料相談はこちら</a>
 </div>
-
-<div class="header__main__tel__link">
-<a class="text-body" href="tel:0120-00-0000">
-<span class="header__main__tel__link--icon"><img class="img-fluid" src="<?php e_img_url(); ?>/icon_tel.png" alt="電話アイコン"></span>
-<span class="header__main__tel__link--num">0120-00-0000</span>
-</a>
-<p class="mb-0 header__main__tel__link--p small text-center mt-2">営業時間：9時〜18時｜定休⽇：水曜日</p>
-</div>
-
-</div>
-
-<div class="header__main__contact">
-<a class="btn btn-primary" href="<?php e_home_url(); ?>/company#form">
-<span class="btn-primary--point">＼ 24時間受付中 ／</span>
-<span class="btn-primary--txt">無料査定・売却相談はこちら</span>
-</a>
-</div>
-
-</div>
-<!-- header__main -->
-
-<nav class="header__nav">
-<ul>
-<li><a href="<?php e_home_url(); ?>">トップページ</a></li>
-<li><a href="<?php e_home_url(); ?>#service">サービス内容</a></li>
-<li><a href="<?php e_home_url(); ?>/works/">買取実績</a></li>
-<li><a href="<?php e_home_url(); ?>#flow">サービスの流れ</a></li>
-<li><a href="<?php e_home_url(); ?>/voice/">お客様の声</a></li>
-<li><a href="<?php e_home_url(); ?>/blog/">ブログ</a></li>
-<li><a href="<?php e_home_url(); ?>/company/">会社案内</a></li>
-</ul>
-</nav>
-<!-- header__nav -->
-
-<div class="header__nav__sp">
-<div class="header__nav__sp__tel">
-<a href="tel:">
-<img class="img-fluid header__nav__sp__tel--img" src="<?php e_img_url(); ?>/icon_tel_sp.png" alt="電話">
-<span class="header__nav__sp__tel--txt">電話相談</span>
-</a>
-</div>
-<!-- header__nav__sp__tel -->
-<div class="header__nav__sp__toggle">
-<span class="header__nav__sp__toggle--hum"></span>
-<span class="header__nav__sp__toggle--txt">MENU</span>
-</div>
-<!-- header__nav__sp__toggle -->
-<nav class="header__nav__sp__menu">
-<ul class="header__nav__sp__menu--ul">
-<li><a href="<?php e_home_url(); ?>">トップページ</a></li>
-<li><a href="<?php e_home_url(); ?>#service">サービス内容</a></li>
-<li><a href="<?php e_home_url(); ?>/works/">買取実績</a></li>
-<li><a href="<?php e_home_url(); ?>#flow">サービスの流れ</a></li>
-<li><a href="<?php e_home_url(); ?>/voice/">お客様の声</a></li>
-<li><a href="<?php e_home_url(); ?>/blog/">ブログ</a></li>
-<li><a href="<?php e_home_url(); ?>/company/">会社案内</a></li>
-</ul>
-<div class="header__main__tel__link">
-<a class="text-body" href="tel:0120-00-0000">
-<span class="header__main__tel__link--icon"><img class="img-fluid" src="<?php e_img_url(); ?>/icon_tel.png" alt="電話アイコン"></span>
-<span class="header__main__tel__link--num">0120-00-0000</span>
-</a>
-<p class="mb-0 header__main__tel__link--p small text-center mt-2">営業時間：9時〜18時｜定休⽇：水曜日</p>
-</div>
-<div class="text-center pb-4">
-<a class="btn btn-primary" href="<?php e_home_url(); ?>/company#form">
-<span class="btn-primary--point">＼ 24時間受付中 ／</span>
-<span class="btn-primary--txt">無料査定・売却相談はこちら</span>
-</a>
-</div>
-</nav>
-<!-- header__nav__sp__menu -->
-</div>
-<!-- header__nav__sp -->
-
 </div>
 <!-- container -->
-
+</div>
+<!-- header__top -->
+<nav class="header__nav">
+<ul class="container">
+<li><a href="">ホーム</a></li>
+<li><a href="">料金表</a></li>
+<li><a href="">サービス内容</a></li>
+<li><a href="">スタッフ紹介</a></li>
+<li><a href="">会社案内</a></li>
+<li><a href="">お役立ち情報</a></li>
+<li><a href="">提携業者募集</a></li>
+<li><a href="">ブログ</a></li>
+</ul>
+</nav>
 </header>
 
 <main>
