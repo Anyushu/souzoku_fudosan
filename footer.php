ff4942f5e027a8664e40f91bde520323a93ad16a
ShunyaSuzuki <anyushu2017@gmail.com>
2020-04-26 22:42:06 +0900

    update

--- footer.php
+++ footer.php
@@ -1,7 +1,75 @@
 </main>
 
-<footer></footer>
+<section class="cta sec bg-square">
+<div class="container">
+<h2 class="ttl__h2 text-center">お問い合わせ</h2>
+<div class="cta__inner">
+<div class="cta__inner__left">
+<div class="cta__inner__left__img">
+<img src="<?php e_img_url(); ?>/foot_contact_img.png" alt="">
+</div>
+<p class="cta__inner__left--ttl">谷町四丁目駅から徒歩2分</p>
+<p class="cta__inner__left--txt">大阪府大阪市中央区谷町2丁目4-3 アイエスビル5F</p>
+</div>
+<!-- cta__inner__left -->
+<div class="cta__inner__right">
+<div class="cta__inner__right__balloon">何度でも<span>相談料無料</span>なので<br>お気軽にお問い合わせください。</div>
+<a class="cta__inner__right__tel" href="tel:0120-200-567">
+<span class="cta__inner__right__tel--icon"><img src="<?php e_img_url(); ?>/icon_free.png" alt="フリーダイヤル"></span>
+<span class="cta__inner__right__tel--num">0120-200-567<span class="cta__inner__right__tel--txt">営業時間：10:00 〜19:00｜定休日：水曜日</span></span>
+</a>
+<p class="cta__inner__right--txt">お電話が苦手という方はまずはメールにてご相談してみてください。</p>
+<div class="cta__inner__right__btn">
+<a class="btn btn-primary font-weight-bold text-white w-100" href="<?php e_home_url(); ?>/contact/"><i class="far fa-envelope mr-2"></i>無料相談はこちら</a>
+</div>
+</div>
+<!-- cta__inner__right -->
+</div>
+</div>
+</section>
 
+<footer class="footer">
+<div class="container">
+<div class="footer__wrap">
+<div class="footer__wrap__logo">
+<a class="footer__wrap__logo--link m-plus" href="<?php e_home_url(); ?>">相続不動産サポートセンター</a>
+<div class="footer__wrap__logo__tel">
+<a href="tel:0120-200-567">
+<img src="<?php e_img_url(); ?>/icon_free_white.png" alt="">
+<span>0120-200-567</span>
+<p>営業時間：10:00 〜19:00 ｜ 定休日：水曜日</p>
+</a>
+</div>
+</div>
+<!-- footer__wrap__logo -->
+<div class="footer__wrap__links">
+<ul>
+<li><a href="<?php e_home_url(); ?>/">ホーム</a></li>
+<li><a href="<?php e_home_url(); ?>/reason/">選ばれる理由</a></li>
+<li><a href="<?php e_home_url(); ?>/service/">サービス内容</a></li>
+<li><a href="<?php e_home_url(); ?>/staff/">スタッフ紹介</a></li>
+<li><a href="<?php e_home_url(); ?>/company/">会社案内</a></li>
+</ul>
+<ul>
+<li><a href="<?php e_home_url(); ?>">相続トラブルでお困りの方へ</a></li>
+<li><a href="<?php e_home_url(); ?>">相続手続きについて</a></li>
+<li><a href="<?php e_home_url(); ?>">相続税について</a></li>
+<li><a href="<?php e_home_url(); ?>">相続不動産の管理・清掃について</a></li>
+<li><a href="<?php e_home_url(); ?>">遺品整理について</a></li>
+</ul>
+<ul>
+<li><a href="<?php e_home_url(); ?>">不用品回収について</a></li>
+<li><a href="<?php e_home_url(); ?>">空家買取について</a></li>
+<li><a href="<?php e_home_url(); ?>">提携業者募集</a></li>
+<li><a href="<?php e_home_url(); ?>">ブログ</a></li>
+<li><a href="<?php e_home_url(); ?>">無料相談はこちら</a></li>
+</ul>
+</div>
+</div>
+<!-- footer__wrap -->
+</div>
+</footer>
+<p class="socket">©2020 Shineijyutaku</p>
 <?php wp_footer(); ?>
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -157,4 +157,409 @@
 <!-- container -->
 </section>
 <!-- sec -->
+
+<section class="sec contact bg-square">
+<div class="container">
+<p class="contact__p">下記相続に関してお悩みの方はお気軽にご相談ください！</p>
+<h2 class="contact__ttl"><mark>何度でも相談料無料です！</mark></h2>
+<div class="contact__list">
+<ul>
+<li>相続財産の調査をしてほしい方</li>
+<li>相続手続きでお悩みの方</li>
+<li>相続申告が必要な方</li>
+<li>相続放棄をお考えの方</li>
+<li>不動産の名義変更が必要な方</li>
+<li>不動産をすぐ売却したい方</li>
+</ul>
+<ul>
+<li>他の相続人が誰なのか調べてほしい方</li>
+<li>年金の手続きをしてほしい方</li>
+<li>生前贈与をお考えの方</li>
+<li>遺言書について知りたい方</li>
+<li>相続税について知りたい方</li>
+<li>事業継承対策をお考えの方</li>
+</ul>
+<img class="contact__list--img" src="<?php e_img_url(); ?>/contact_pic.png" alt="">
+</div>
+<!-- contact__list -->
+<div class="contact__link">
+<div class="contact__link__tel">
+<div class="contact__link__tel--num">
+<a href="tel:0120-200-567">
+<img src="<?php e_img_url(); ?>/icon_free_white.png" alt="">
+<span>0120-200-567</span>
+<p>営業時間：10:00 〜19:00 ｜ 定休日：水曜日</p>
+</a>
+</div>
+</div>
+<!-- contact__link__tel -->
+<div class="contact__link__btn">
+<p>お電話が苦手という方は<br>まずはメールにてご相談してみてください。</p>
+<a class="btn btn-light d-block" href="<?php e_home_url(); ?>/contatc/"><i class="far fa-envelope mr-2"></i>無料相談はこちら</a>
+</div>
+<!-- contact__link__btn -->
+</div>
+</div>
+</section>
+
+<section class="sec case">
+<div class="container">
+<h2 class="ttl__h2 text-center">売却例</h2>
+<div class="case__list">
+<div class="case__list__inner">
+<a href="">
+<h3 class="case__list__inner__ttl">
+<span class="text-primary">ケース1</span>
+<span class="badge badge-pill badge-primary case__list__inner__ttl--area">大阪市浪速区</span>
+</h3>
+<div class="case__list__inner__info">
+<div class="case__list__inner__info__img">
+<img src="<?php e_img_url(); ?>/case_img_1.png" alt="">
+</div>
+<div class="case__list__inner__info__txt">
+<p>物件種別：マンション
+<br>築年数：60年
+<br>面積：60㎡
+<br>期間：2ヶ月
+<br>売却額：<span class="text-primary">2800万</span></p>
+</div>
+</div>
+</a>
+</div>
+<!-- case__list__inner -->
+<div class="case__list__inner">
+<a href="">
+<h3 class="case__list__inner__ttl">
+<span class="text-primary">ケース2</span>
+<span class="badge badge-pill badge-primary case__list__inner__ttl--area">大阪市浪速区</span>
+</h3>
+<div class="case__list__inner__info">
+<div class="case__list__inner__info__img">
+<img src="<?php e_img_url(); ?>/case_img_2.png" alt="">
+</div>
+<div class="case__list__inner__info__txt">
+<p>物件種別：マンション
+<br>築年数：60年
+<br>面積：60㎡
+<br>期間：2ヶ月
+<br>売却額：<span class="text-primary">2800万</span></p>
+</div>
+</div>
+</a>
+</div>
+<!-- case__list__inner -->
+<div class="case__list__inner mb-md-0">
+<a href="">
+<h3 class="case__list__inner__ttl">
+<span class="text-primary">ケース3</span>
+<span class="badge badge-pill badge-primary case__list__inner__ttl--area">大阪市浪速区</span>
+</h3>
+<div class="case__list__inner__info">
+<div class="case__list__inner__info__img">
+<img src="<?php e_img_url(); ?>/case_img_3.png" alt="">
+</div>
+<div class="case__list__inner__info__txt">
+<p>物件種別：マンション
+<br>築年数：60年
+<br>面積：60㎡
+<br>期間：2ヶ月
+<br>売却額：<span class="text-primary">2800万</span></p>
+</div>
+</div>
+</a>
+</div>
+<!-- case__list__inner -->
+<div class="case__list__inner mb-0">
+<a href="">
+<h3 class="case__list__inner__ttl">
+<span class="text-primary">ケース4</span>
+<span class="badge badge-pill badge-primary case__list__inner__ttl--area">大阪市浪速区</span>
+</h3>
+<div class="case__list__inner__info">
+<div class="case__list__inner__info__img">
+<img src="<?php e_img_url(); ?>/case_img_4.png" alt="">
+</div>
+<div class="case__list__inner__info__txt">
+<p>物件種別：マンション
+<br>築年数：60年
+<br>面積：60㎡
+<br>期間：2ヶ月
+<br>売却額：<span class="text-primary">2800万</span></p>
+</div>
+</div>
+</a>
+</div>
+<!-- case__list__inner -->
+</div>
+<!-- case__list -->
+</div>
+<!-- container -->
+</section>
+
+<section class="sec links">
+<div class="container">
+<div class="links__list">
+<a class="links__list__inner" href="<?php e_home_url(); ?>/price/">
+<div class="links__list__inner__txt">
+<p class="links__list__inner__txt--ttl">料金表</p>
+<p class="links__list__inner__txt--p">相続に関する相談料は無料です。<br>この機会にご相談してみてください。</p>
+</div>
+<div class="links__list__inner__img">
+<img src="<?php e_img_url(); ?>/illust_price.png" alt="">
+</div>
+</a>
+<!-- links__list__inner -->
+<a class="links__list__inner" href="<?php e_home_url(); ?>/service/">
+<div class="links__list__inner__txt">
+<p class="links__list__inner__txt--ttl">サービス内容</p>
+<p class="links__list__inner__txt--p">相続不動産サポートセンターは相続の<br>専門家と不動産の専門家、両方が在籍<br>している他にない体制を構築しています。</p>
+</div>
+<div class="links__list__inner__img">
+<img src="<?php e_img_url(); ?>/illust_service.png" alt="">
+</div>
+</a>
+<!-- links__list__inner -->
+<a class="links__list__inner" href="<?php e_home_url(); ?>/price/">
+<div class="links__list__inner__txt">
+<p class="links__list__inner__txt--ttl">スタッフ紹介</p>
+<p class="links__list__inner__txt--p">相続不動産サポートセンターに<br>在籍している専門家などのスタッフを<br>ご紹介いたします。</p>
+</div>
+<div class="links__list__inner__img">
+<img src="<?php e_img_url(); ?>/illust_staff.png" alt="">
+</div>
+</a>
+<!-- links__list__inner -->
+<a class="links__list__inner" href="<?php e_home_url(); ?>/price/">
+<div class="links__list__inner__txt">
+<p class="links__list__inner__txt--ttl">提携業者募集</p>
+<p class="links__list__inner__txt--p">相続不動産サポートセンターでは<br>随時提携業者を募集しています。<br>興味がございましたら、ご覧ください。</p>
+</div>
+<div class="links__list__inner__img">
+<img src="<?php e_img_url(); ?>/illust_alliance.png" alt="">
+</div>
+</a>
+<!-- links__list__inner -->
+<a class="links__list__inner" href="<?php e_home_url(); ?>/price/">
+<div class="links__list__inner__txt">
+<p class="links__list__inner__txt--ttl">会社案内</p>
+<p class="links__list__inner__txt--p">相続不動産サポートセンターを運営している<br>株式会社シンエイの会社情報をご案内いたします。</p>
+</div>
+<div class="links__list__inner__img">
+<img src="<?php e_img_url(); ?>/illust_company.png" alt="">
+</div>
+</a>
+<!-- links__list__inner -->
+</div>
+</div>
+</section>
+
+<section class="sec useful">
+<div class="container">
+<h2 class="ttl__h2 text-center">お役立ち情報</h2>
+<div class="useful__list">
+<div class="useful__list__inner">
+<h3 class="useful__list__inner__ttl">相続トラブルでお困りの方へ</h3>
+<div class="useful__list__inner__info">
+<div class="useful__list__inner__info__img">
+<img src="<?php e_img_url(); ?>/useful_img_1.png" alt="">
+</div>
+<div class="useful__list__inner__info__txt">
+<p>大阪府内で相続トラブルでお困りの方はこちらのページをご覧ください。きっと今お抱えの悩みは解決されると思います。</p>
+<a href="">
+<i class="fas fa-caret-right"></i>
+<span>詳しくはこちら</span>
+</a>
+</div>
+</div>
+</div>
+<!-- useful__list__inner -->
+<div class="useful__list__inner">
+<h3 class="useful__list__inner__ttl">相続手続きについて</h3>
+<div class="useful__list__inner__info">
+<div class="useful__list__inner__info__img">
+<img src="<?php e_img_url(); ?>/useful_img_2.png" alt="">
+</div>
+<div class="useful__list__inner__info__txt">
+<p>大阪府内で相続手続きをこれからされる方、今後する予定の方、している最中で分からなくなった方、こちらのページをご覧ください。</p>
+<a href=""><i class="fas fa-caret-right"></i><span>詳しくはこちら</span></a>
+</div>
+</div>
+</div>
+<!-- useful__list__inner -->
+<div class="useful__list__inner">
+<h3 class="useful__list__inner__ttl">相続税について</h3>
+<div class="useful__list__inner__info">
+<div class="useful__list__inner__info__img">
+<img src="<?php e_img_url(); ?>/useful_img_3.png" alt="">
+</div>
+<div class="useful__list__inner__info__txt">
+<p>相続税は非常に複雑で一般の方からすると日常的なことではないので、悩まれる方が非常に多いです。このページでは、相続税について分かりやすく解決しています。</p>
+<a href=""><i class="fas fa-caret-right"></i><span>詳しくはこちら</span></a>
+</div>
+</div>
+</div>
+<!-- useful__list__inner -->
+<div class="useful__list__inner">
+<h3 class="useful__list__inner__ttl">相続不動産の管理・清掃について</h3>
+<div class="useful__list__inner__info">
+<div class="useful__list__inner__info__img">
+<img src="<?php e_img_url(); ?>/useful_img_4.png" alt="">
+</div>
+<div class="useful__list__inner__info__txt">
+<p>相続不動産で問題なる一つに物件の管理や清掃はどうなるのかという問題です。この問題も非常に悩ましいことですが、このぺージで詳しく解説しているので、ご覧ください。</p>
+<a href=""><i class="fas fa-caret-right"></i><span>詳しくはこちら</span></a>
+</div>
+</div>
+</div>
+<!-- useful__list__inner -->
+<div class="useful__list__inner">
+<h3 class="useful__list__inner__ttl">遺品整理について</h3>
+<div class="useful__list__inner__info">
+<div class="useful__list__inner__info__img">
+<img src="<?php e_img_url(); ?>/useful_img_5.png" alt="">
+</div>
+<div class="useful__list__inner__info__txt">
+<p>相続不動産に関連する遺品整理ですが、相続不動産の相談や手続きができる業者で遺品整理までできる業者は少ないです。当社は遺品整理もできます。(大阪府内のみ)このぺージで説明しているので、ご覧ください。</p>
+<a href=""><i class="fas fa-caret-right"></i><span>詳しくはこちら</span></a>
+</div>
+</div>
+</div>
+<!-- useful__list__inner -->
+<div class="useful__list__inner">
+<h3 class="useful__list__inner__ttl">不用品回収について</h3>
+<div class="useful__list__inner__info">
+<div class="useful__list__inner__info__img">
+<img src="<?php e_img_url(); ?>/useful_img_6.png" alt="">
+</div>
+<div class="useful__list__inner__info__txt">
+<p>相続不動産に関連する不用品回収ですが、相続不動産の相談や手続きができる業者で不用品回収までできる業者は少ないです。当社は不用品回収もできます。(大阪府内のみ)このぺージで説明しているので、ご覧ください。</p>
+<a href=""><i class="fas fa-caret-right"></i><span>詳しくはこちら</span></a>
+</div>
+</div>
+</div>
+<!-- useful__list__inner -->
+<div class="useful__list__inner">
+<h3 class="useful__list__inner__ttl">空家買取について</h3>
+<div class="useful__list__inner__info">
+<div class="useful__list__inner__info__img">
+<img src="<?php e_img_url(); ?>/useful_img_7.png" alt="">
+</div>
+<div class="useful__list__inner__info__txt">
+<p>当社は空家買取本舗というサイトを運営しております。昨今の日本で社会問題にもなっている空き家問題。
+<br>この問題を解決すべく当サービスを提供しています。空き家買取は単純な問題ではなく、色んな要素が複雑に絡み合った、非常に込み入った問題です。それを当社ではゼロからご相談にのり、問題を解決しています。
+<br>そんな空き家買取のサービス内容や情報をご案内しておりますので、ご覧ください。</p>
+<a href=""><i class="fas fa-caret-right"></i><span>詳しくはこちら</span></a>
+</div>
+</div>
+</div>
+<!-- useful__list__inner -->
+</div>
+<!-- useful__list -->
+</div>
+</section>
+
+<section class="blog sec">
+<div class="container">
+<h2 class="ttl__h2 text-center">ブログ</h2>
+<div class="blog__list">
+<article class="blog__list__inner">
+<a href="">
+<div class="blog__list__inner__meta">
+<span class="blog__list__inner__meta--time">2019.09.30</span>
+<span class="blog__list__inner__meta--cat">カテゴリー</span>
+</div>
+<div class="blog__list__inner__img">
+<img src="<?php e_img_url(); ?>/no_img.png" alt="">
+</div>
+<div class="blog__list__inner__ttl">
+<p>タイトルタイトルタイトルタイトルタイトルタイトル</p>
+</div>
+</a>
+</article>
+<article class="blog__list__inner">
+<a href="">
+<div class="blog__list__inner__meta">
+<span class="blog__list__inner__meta--time">2019.09.30</span>
+<span class="blog__list__inner__meta--cat">カテゴリー</span>
+</div>
+<div class="blog__list__inner__img">
+<img src="<?php e_img_url(); ?>/no_img.png" alt="">
+</div>
+<div class="blog__list__inner__ttl">
+<p>タイトルタイトルタイトルタイトルタイトルタイトル</p>
+</div>
+</a>
+</article>
+<article class="blog__list__inner">
+<a href="">
+<div class="blog__list__inner__meta">
+<span class="blog__list__inner__meta--time">2019.09.30</span>
+<span class="blog__list__inner__meta--cat">カテゴリー</span>
+</div>
+<div class="blog__list__inner__img">
+<img src="<?php e_img_url(); ?>/no_img.png" alt="">
+</div>
+<div class="blog__list__inner__ttl">
+<p>タイトルタイトルタイトルタイトルタイトルタイトル</p>
+</div>
+</a>
+</article>
+<article class="blog__list__inner">
+<a href="">
+<div class="blog__list__inner__meta">
+<span class="blog__list__inner__meta--time">2019.09.30</span>
+<span class="blog__list__inner__meta--cat">カテゴリー</span>
+</div>
+<div class="blog__list__inner__img">
+<img src="<?php e_img_url(); ?>/no_img.png" alt="">
+</div>
+<div class="blog__list__inner__ttl">
+<p>タイトルタイトルタイトルタイトルタイトルタイトル</p>
+</div>
+</a>
+</article>
+</div>
+<!-- blog__list -->
+<div class="mt-5 text-center">
+<a class="btn btn-secondary" href="<?php e_home_url(); ?>/blog/"><i class="fas fa-caret-right"></i>一覧を見る</a>
+</div>
+</div>
+</section>
+
+<section class="area sec">
+<div class="container position-relative">
+<h2 class="ttl__h2 text-center">対応エリア</h2>
+<div class="area__list">
+<h3>大阪府全域</h3>
+<p>大阪市(⻄区-浪速区- ⻄成区- 港区- 此花区- ⻄淀川区- 東淀川区- 東成区- 天王寺区-住吉区-大正区- 旭区-福島区- 都島区- 城東区- 生野区- 阿倍野区- 東住吉区-淀川区-鶴見区- 平野区- 住之江区-中央区- 北区)堺市( 中区- 東区- ⻄区- 南区- 北区- 美原区)豊中市,高槻市,枚方市,東大阪市,吹田市,茨木市,岸和田市,寝屋川市,八尾市,池田市,箕面市,豊能郡,能勢町,豊能町,摂津市,三島郡,島本町,泉大津市,和泉市,高石市,泉北郡,忠岡町,貝塚市,泉佐野市,泉南市,阪南市,泉南郡,熊取町,田尻町,岬町,守口市,大東市,門真市,四條畷市,交野市,柏原市,富田林市,河内⻑野市,松原市,羽曳野市,藤井寺市,大阪狭山市,南河内郡,太子町,河南町,千早赤阪村</p>
+</div>
+<img class="area__map" src="<?php e_img_url(); ?>/area_map.png" alt="">
+</div>
+</section>
+
+<section class="message">
+<div class="conatiner">
+<h2 class="message__ttl">
+<span>相続不動産サポートセンターの想い</span>
+</h2>
+<p class="message__txt">相続不動産サポートセンターは、相続の専門家、不動産の専門家が多数在籍しておりまして、
+<br>相続から不動産売却まで一気通貫でサポートするサービスをご提供しております。
+<br>元々は不動産をメインに活動しておりましたが、年々相続からの不動産売却というご相談が増えており、
+<br>この機会に相続不動産に特化したサポートサービスを立ち上げることになりました。
+<br>相続は非常に複雑で不透明な業界だと思います。
+<br>ほとんどの会社は士業が運営しているケースが多く、不動産売却は他社に丸投げというのが業界の通例であります。
+<br>ただ、この流れはお客様にとってあまり良いものではありません。
+<br>余分な費用、余分な時間がかかり、本来お客様が解決したい相続の悩みが解決できていないということが多々起きています。
+<br>そんな問題を解決したいという想いもあり、立ち上げました。
+<br>当社は一気通貫でサポートしているため、余分な費用や時間がかからず、
+<br>本来解決したい問題を円滑に解決することができます。
+<br>その結果、相談件数も1000件以上になり、お客様から大変高評価をいただいております。
+<br>また、何度でも相談料無料の取り組みや着手金無料の取り組みなどもお客様の相談ハードルを下げている要因だと考えております。
+<br>現在は大阪のみの対応エリアですが、今後エリアを拡大し、
+<br>当社のサービスを1人でも多くの相続に悩まれている方に提供していきたいと考えております。
+<br>今後とも相続不動産サポートセンターを何卒よろしくお願い申し上げます。</p>
+</div>
+<img class="w-100" src="<?php e_img_url(); ?>/seo_bottom_pc.png" alt="">
+</section>
+
 <?php get_footer();
